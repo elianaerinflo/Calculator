6ba8ae9b10d8f9aa4fb38e3d729cbeff56558db7
Eleana Erin Flores <160589391+elianaerinflo@users.noreply.github.com>
2024-02-25 20:58:35 +0800

    Add files via upload

---
+++ CALCU (MINOR A)/Form1.Designer.vb
@@ -0,0 +1,343 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnNum1 = New System.Windows.Forms.Button()
+        Me.btnNum2 = New System.Windows.Forms.Button()
+        Me.btnNum3 = New System.Windows.Forms.Button()
+        Me.btnNum0 = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDec = New System.Windows.Forms.Button()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        Me.btnPosNeg = New System.Windows.Forms.Button()
+        Me.btnNum4 = New System.Windows.Forms.Button()
+        Me.btnNum5 = New System.Windows.Forms.Button()
+        Me.btnNum6 = New System.Windows.Forms.Button()
+        Me.btnSub = New System.Windows.Forms.Button()
+        Me.btnMul = New System.Windows.Forms.Button()
+        Me.btnNum9 = New System.Windows.Forms.Button()
+        Me.btnNum8 = New System.Windows.Forms.Button()
+        Me.btnNum7 = New System.Windows.Forms.Button()
+        Me.btnSquared = New System.Windows.Forms.Button()
+        Me.btnCE = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.txtBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnNum1
+        '
+        Me.btnNum1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum1.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum1.Location = New System.Drawing.Point(37, 305)
+        Me.btnNum1.Name = "btnNum1"
+        Me.btnNum1.Size = New System.Drawing.Size(71, 63)
+        Me.btnNum1.TabIndex = 1
+        Me.btnNum1.Text = "1"
+        Me.btnNum1.UseVisualStyleBackColor = True
+        '
+        'btnNum2
+        '
+        Me.btnNum2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum2.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum2.Location = New System.Drawing.Point(114, 305)
+        Me.btnNum2.Name = "btnNum2"
+        Me.btnNum2.Size = New System.Drawing.Size(71, 63)
+        Me.btnNum2.TabIndex = 1
+        Me.btnNum2.Text = "2"
+        Me.btnNum2.UseVisualStyleBackColor = True
+        '
+        'btnNum3
+        '
+        Me.btnNum3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum3.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum3.Location = New System.Drawing.Point(191, 305)
+        Me.btnNum3.Name = "btnNum3"
+        Me.btnNum3.Size = New System.Drawing.Size(72, 63)
+        Me.btnNum3.TabIndex = 1
+        Me.btnNum3.Text = "3"
+        Me.btnNum3.UseVisualStyleBackColor = True
+        '
+        'btnNum0
+        '
+        Me.btnNum0.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum0.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum0.Location = New System.Drawing.Point(114, 374)
+        Me.btnNum0.Name = "btnNum0"
+        Me.btnNum0.Size = New System.Drawing.Size(71, 63)
+        Me.btnNum0.TabIndex = 1
+        Me.btnNum0.Text = "0"
+        Me.btnNum0.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnAdd.BackColor = System.Drawing.Color.Linen
+        Me.btnAdd.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(269, 305)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(70, 63)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnDec
+        '
+        Me.btnDec.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnDec.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDec.Location = New System.Drawing.Point(191, 374)
+        Me.btnDec.Name = "btnDec"
+        Me.btnDec.Size = New System.Drawing.Size(72, 63)
+        Me.btnDec.TabIndex = 1
+        Me.btnDec.Text = "."
+        Me.btnDec.UseVisualStyleBackColor = True
+        '
+        'btnEqual
+        '
+        Me.btnEqual.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnEqual.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEqual.Location = New System.Drawing.Point(269, 374)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(70, 63)
+        Me.btnEqual.TabIndex = 1
+        Me.btnEqual.Text = "="
+        Me.btnEqual.UseVisualStyleBackColor = True
+        '
+        'btnPosNeg
+        '
+        Me.btnPosNeg.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnPosNeg.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPosNeg.Location = New System.Drawing.Point(37, 374)
+        Me.btnPosNeg.Name = "btnPosNeg"
+        Me.btnPosNeg.Size = New System.Drawing.Size(71, 63)
+        Me.btnPosNeg.TabIndex = 1
+        Me.btnPosNeg.Text = "±"
+        Me.btnPosNeg.UseVisualStyleBackColor = True
+        '
+        'btnNum4
+        '
+        Me.btnNum4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum4.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum4.Location = New System.Drawing.Point(37, 236)
+        Me.btnNum4.Name = "btnNum4"
+        Me.btnNum4.Size = New System.Drawing.Size(71, 63)
+        Me.btnNum4.TabIndex = 1
+        Me.btnNum4.Text = "4"
+        Me.btnNum4.UseVisualStyleBackColor = True
+        '
+        'btnNum5
+        '
+        Me.btnNum5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum5.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum5.Location = New System.Drawing.Point(114, 240)
+        Me.btnNum5.Name = "btnNum5"
+        Me.btnNum5.Size = New System.Drawing.Size(71, 59)
+        Me.btnNum5.TabIndex = 1
+        Me.btnNum5.Text = "5"
+        Me.btnNum5.UseVisualStyleBackColor = True
+        '
+        'btnNum6
+        '
+        Me.btnNum6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum6.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum6.Location = New System.Drawing.Point(191, 240)
+        Me.btnNum6.Name = "btnNum6"
+        Me.btnNum6.Size = New System.Drawing.Size(72, 59)
+        Me.btnNum6.TabIndex = 1
+        Me.btnNum6.Text = "6"
+        Me.btnNum6.UseVisualStyleBackColor = True
+        '
+        'btnSub
+        '
+        Me.btnSub.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnSub.BackColor = System.Drawing.Color.Linen
+        Me.btnSub.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSub.Location = New System.Drawing.Point(269, 240)
+        Me.btnSub.Name = "btnSub"
+        Me.btnSub.Size = New System.Drawing.Size(70, 59)
+        Me.btnSub.TabIndex = 1
+        Me.btnSub.Text = "-"
+        Me.btnSub.UseVisualStyleBackColor = False
+        '
+        'btnMul
+        '
+        Me.btnMul.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnMul.BackColor = System.Drawing.Color.Linen
+        Me.btnMul.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMul.Location = New System.Drawing.Point(269, 171)
+        Me.btnMul.Name = "btnMul"
+        Me.btnMul.Size = New System.Drawing.Size(70, 59)
+        Me.btnMul.TabIndex = 1
+        Me.btnMul.Text = "*"
+        Me.btnMul.UseVisualStyleBackColor = False
+        '
+        'btnNum9
+        '
+        Me.btnNum9.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum9.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum9.Location = New System.Drawing.Point(191, 171)
+        Me.btnNum9.Name = "btnNum9"
+        Me.btnNum9.Size = New System.Drawing.Size(72, 59)
+        Me.btnNum9.TabIndex = 1
+        Me.btnNum9.Text = "9"
+        Me.btnNum9.UseVisualStyleBackColor = True
+        '
+        'btnNum8
+        '
+        Me.btnNum8.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum8.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum8.Location = New System.Drawing.Point(114, 171)
+        Me.btnNum8.Name = "btnNum8"
+        Me.btnNum8.Size = New System.Drawing.Size(71, 59)
+        Me.btnNum8.TabIndex = 1
+        Me.btnNum8.Text = "8"
+        Me.btnNum8.UseVisualStyleBackColor = True
+        '
+        'btnNum7
+        '
+        Me.btnNum7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNum7.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum7.Location = New System.Drawing.Point(37, 169)
+        Me.btnNum7.Name = "btnNum7"
+        Me.btnNum7.Size = New System.Drawing.Size(71, 61)
+        Me.btnNum7.TabIndex = 1
+        Me.btnNum7.Text = "7"
+        Me.btnNum7.UseVisualStyleBackColor = True
+        '
+        'btnSquared
+        '
+        Me.btnSquared.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnSquared.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSquared.Location = New System.Drawing.Point(37, 102)
+        Me.btnSquared.Name = "btnSquared"
+        Me.btnSquared.Size = New System.Drawing.Size(71, 61)
+        Me.btnSquared.TabIndex = 1
+        Me.btnSquared.Text = "x²"
+        Me.btnSquared.UseVisualStyleBackColor = True
+        '
+        'btnCE
+        '
+        Me.btnCE.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnCE.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCE.Location = New System.Drawing.Point(114, 102)
+        Me.btnCE.Name = "btnCE"
+        Me.btnCE.Size = New System.Drawing.Size(71, 61)
+        Me.btnCE.TabIndex = 1
+        Me.btnCE.Text = "CE"
+        Me.btnCE.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnClear.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(191, 102)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(72, 61)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "C"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnDiv.BackColor = System.Drawing.Color.Linen
+        Me.btnDiv.Font = New System.Drawing.Font("Sitka Small", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDiv.Location = New System.Drawing.Point(269, 102)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(70, 61)
+        Me.btnDiv.TabIndex = 1
+        Me.btnDiv.Text = "÷"
+        Me.btnDiv.UseVisualStyleBackColor = False
+        '
+        'txtBox1
+        '
+        Me.txtBox1.BackColor = System.Drawing.Color.Linen
+        Me.txtBox1.Font = New System.Drawing.Font("Sitka Small", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBox1.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.txtBox1.Location = New System.Drawing.Point(37, 52)
+        Me.txtBox1.Multiline = True
+        Me.txtBox1.Name = "txtBox1"
+        Me.txtBox1.Size = New System.Drawing.Size(302, 44)
+        Me.txtBox1.TabIndex = 2
+        Me.txtBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.DarkGray
+        Me.ClientSize = New System.Drawing.Size(398, 489)
+        Me.Controls.Add(Me.txtBox1)
+        Me.Controls.Add(Me.btnNum7)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCE)
+        Me.Controls.Add(Me.btnSquared)
+        Me.Controls.Add(Me.btnNum8)
+        Me.Controls.Add(Me.btnNum9)
+        Me.Controls.Add(Me.btnMul)
+        Me.Controls.Add(Me.btnSub)
+        Me.Controls.Add(Me.btnNum6)
+        Me.Controls.Add(Me.btnNum5)
+        Me.Controls.Add(Me.btnNum4)
+        Me.Controls.Add(Me.btnPosNeg)
+        Me.Controls.Add(Me.btnEqual)
+        Me.Controls.Add(Me.btnDec)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnNum0)
+        Me.Controls.Add(Me.btnNum3)
+        Me.Controls.Add(Me.btnNum2)
+        Me.Controls.Add(Me.btnNum1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CALCULATOR"
+        Me.TransparencyKey = System.Drawing.Color.White
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnNum1 As Button
+    Friend WithEvents btnNum2 As Button
+    Friend WithEvents btnNum3 As Button
+    Friend WithEvents btnNum0 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDec As Button
+    Friend WithEvents btnEqual As Button
+    Friend WithEvents btnPosNeg As Button
+    Friend WithEvents btnNum4 As Button
+    Friend WithEvents btnNum5 As Button
+    Friend WithEvents btnNum6 As Button
+    Friend WithEvents btnSub As Button
+    Friend WithEvents btnMul As Button
+    Friend WithEvents btnNum9 As Button
+    Friend WithEvents btnNum8 As Button
+    Friend WithEvents btnNum7 As Button
+    Friend WithEvents btnSquared As Button
+    Friend WithEvents btnCE As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents txtBox1 As TextBox
+End Class
---
+++ CALCU (MINOR A)/Form1.vb
@@ -0,0 +1,263 @@
+﻿Public Class Form1
+    'the global variables that is going to be used throughout the form  
+    Dim num1 As Double
+    Dim num2 As Double
+    Dim ans As Double
+    'the global variable for the operators: Add,sub,Mul and Div
+    Dim opr As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnMulti_Click(sender As Object, e As EventArgs) Handles btnMul.Click, btnAdd.Click, btnDiv.Click, btnSub.Click
+        Dim num As Button = sender
+
+        num1 = Convert.ToDouble(txtBox1.Text)
+
+        If (num.Text = "÷") Then
+            opr = "/"
+        Else
+            opr = num.Text
+        End If
+
+        txtBox1.Text = txtBox1.Text + num.Text
+
+        btnDec.Enabled = True
+
+    End Sub
+
+    Private Sub btnNum0_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnNum0.Click
+        'BTN NUM0
+        txtBox1.Text = txtBox1.Text & "0"
+    End Sub
+
+    Private Sub btnNum1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnNum1.Click
+        'BTN NUM1
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "1"
+        Else
+            txtBox1.Text = txtBox1.Text & "1"
+        End If
+    End Sub
+
+    Private Sub btnNum2_Click(sender As Object, e As EventArgs) Handles btnNum2.Click
+        'BTN NUM2
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "2"
+        Else
+            txtBox1.Text = txtBox1.Text & "2"
+        End If
+    End Sub
+
+    Private Sub btnNum3_Click(sender As Object, e As EventArgs) Handles btnNum3.Click
+        'BTN NUM3
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "3"
+        Else
+            txtBox1.Text = txtBox1.Text & "3"
+        End If
+    End Sub
+
+    Private Sub btnNum4_Click(sender As Object, e As EventArgs) Handles btnNum4.Click
+        'BTN NUM4
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "4"
+        Else
+            txtBox1.Text = txtBox1.Text & "4"
+        End If
+    End Sub
+
+    Private Sub btnNum5_Click(sender As Object, e As EventArgs) Handles btnNum5.Click
+        'BTN NUM5
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "5"
+        Else
+            txtBox1.Text = txtBox1.Text & "5"
+        End If
+    End Sub
+
+    Private Sub btnNum6_Click(sender As Object, e As EventArgs) Handles btnNum6.Click
+        'BTN NUM6
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "6"
+        Else
+            txtBox1.Text = txtBox1.Text & "6"
+        End If
+    End Sub
+
+    Private Sub btnNum7_Click(sender As Object, e As EventArgs) Handles btnNum7.Click
+        'BTN NUM7
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "7"
+        Else
+            txtBox1.Text = txtBox1.Text & "7"
+        End If
+    End Sub
+
+    Private Sub btnNum8_Click(sender As Object, e As EventArgs) Handles btnNum8.Click
+        'BTN NUM8
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "8"
+        Else
+            txtBox1.Text = txtBox1.Text & "8"
+        End If
+    End Sub
+
+    Private Sub btnNum9_Click(sender As Object, e As EventArgs) Handles btnNum9.Click
+        'BTN NUM9
+        If txtBox1.Text = "0" Then
+            txtBox1.Text = "9"
+        Else
+            txtBox1.Text = txtBox1.Text & "9"
+        End If
+    End Sub
+
+    Private Sub btnDec_Click(sender As Object, e As EventArgs) Handles btnDec.Click
+        'for decimal
+
+        If txtBox1.Text = "" Then
+            txtBox1.Text = "0"
+        End If
+
+        If btnDec.Enabled = True Then
+            txtBox1.Text = txtBox1.Text + "."
+            btnDec.Enabled = False
+
+
+        End If
+    End Sub
+
+    Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+
+        'OPERATOR FOR EQUAL BTN: 
+
+        EvaluateEntries()
+
+        If opr = "+" Then
+            ans = num1 + num2
+            txtBox1.Text = Convert.ToString(ans)
+        ElseIf opr = "-" Then
+            ans = num1 - num2
+            txtBox1.Text = Convert.ToString(ans)
+        ElseIf opr = "*" Then
+            ans = num1 * num2
+            txtBox1.Text = Convert.ToString(ans)
+        ElseIf opr = "/" Then
+            ans = num1 / num2
+            txtBox1.Text = Convert.ToString(ans)
+        End If
+
+        btnDec.Enabled = True
+    End Sub
+
+    Private Sub InitializeAllEntry()
+
+        'INITIALIZATION FOR VARIABLES AND OPERATORS
+        ans = 0
+        num1 = 0
+        num2 = 0
+        opr = ""
+        btnDec.Enabled = True
+    End Sub
+
+    Private Sub ClearEntry()
+        Try
+
+            Dim tempOpr As String = ""
+
+            If (opr = "/") Then
+                tempOpr = "÷"
+            Else
+                tempOpr = opr
+            End If
+
+            txtBox1.Text = txtBox1.Text.Substring(0, txtBox1.Text.IndexOf(tempOpr) + 1)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'CLEAR ALL BTN
+        InitializeAllEntry()
+        txtBox1.Text = ""
+    End Sub
+
+    Private Sub btnCE_Click(sender As Object, e As EventArgs) Handles btnCE.Click
+        'CLEAR ENTRY BTN
+        ClearEntry()
+        btnDec.Enabled = True
+    End Sub
+
+    Private Sub EvaluateEntries()
+
+        'THE TRY CATCH IS FOR ERROR, IF TXTBOX CONTAINS ONLY 1 VALUE WITHOUT
+        'OPERATOR THEN IT WILL STAY THE SAME
+
+        Try
+            Dim tempOpr As String = ""
+            If (opr = "/") Then
+                tempOpr = "÷"  'for catch division
+            Else
+                tempOpr = opr '
+            End If
+
+            'FUNCTION
+
+            num1 = txtBox1.Text.Substring(0, txtBox1.Text.IndexOf(tempOpr))
+            'RETRIEVE THE FIRST VARIABLE
+            'using substring function to get the 1st operand which starts with index 0, and ends at the position of the operation symbol
+
+            Dim forNum2 = txtBox1.Text.Substring(txtBox1.Text.IndexOf(tempOpr) + 1, txtBox1.Text.Length - (txtBox1.Text.IndexOf(tempOpr) + 1))
+            'RETRIEVE THE SECOND VARIABLE
+            'THE COUNT STARTS WITH THE OPERATION
+
+            If (forNum2 = "") Then
+                num2 = num1
+                'IF NUM2 IS EMPTY THEN NUM2 WILL ALSO HAVE THE SAME VALUE AS NUM1 
+
+            Else
+                num2 = forNum2
+            End If
+
+        Catch ex As Exception
+
+            If txtBox1.Text IsNot "" Then
+                num1 = txtBox1.Text
+            End If
+
+        End Try
+
+    End Sub
+
+    Private Sub btnSquared_Click(sender As Object, e As EventArgs) Handles btnSquared.Click
+        'SQUAREROOT
+
+
+        If txtBox1.Text = "" Or txtBox1.Text = "." Then
+            Return
+        End If
+
+        num1 = txtBox1.Text
+        ans = txtBox1.Text ^ 2
+        txtBox1.Text = ans
+
+    End Sub
+
+    Private Sub btnPosNeg_Click(sender As Object, e As EventArgs) Handles btnPosNeg.Click
+        'POSITVE, NEGATIVE 
+
+        If txtBox1.Text = "" Or txtBox1.Text = "." Then
+            Return
+        End If
+
+        If (txtBox1.Text.Contains("-")) Then
+            txtBox1.Text = txtBox1.Text.Remove(0, 1)
+
+        ElseIf (txtBox1.Text <> 0) Then
+            txtBox1.Text = "-" + txtBox1.Text
+
+        End If
+    End Sub
+End Class
